ddf23d9a84e28dbcafac55b533f6a6100f3446d3
Andrew Coulton <andrew@ingenerator.com>
2014-01-30 22:36:13 +0000

    Install lockrun to prevent overlapping backup runs
    Track the source directly in the repo, and build it on deployment.

---
+++ recipes/install_lockrun.rb
@@ -0,0 +1,49 @@
+#
+# Installs duplicity from source
+#
+# Author::  Andrew Coulton (<andrew@ingenerator.com>)
+# Cookbook Name:: duplicity-backup
+# Recipe:: install_lockrun
+#
+# Copyright 2013-14, inGenerator Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+src_dir = File.join(node['duplicity']['src_dir'], 'lockrun')
+directory src_dir do
+  action    :create
+  owner     "root"
+  group     "root"
+  mode      0755
+  recursive true
+end
+
+file "#{src_dir}/lockrun.c" do
+  action    :create
+  owner     "root"
+  group     "root"
+  mode      0644  
+end
+
+execute "gcc lockrun.c -o lockrun" do
+  action    :run
+  cwd       src_dir
+  user      "root"
+  not_if    "which lockrun"
+end
+
+link "/usr/local/bin/lockrun" do
+  action      :create
+  target_file "#{src_dir}/lockrun"
+end
---
+++ spec/install_lockrun_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe 'duplicity-backup::install_lockrun' do
+  let (:chef_run) do
+    ChefSpec::Runner.new do | node |
+      # Set non-standard attributes to check the recipe is using the attributes
+      node.set['duplicity']['src_dir'] = '/usr/local/othersrc'
+    end.converge(described_recipe)
+  end    
+
+  before(:each) do
+    stub_command("which lockrun").and_return(true)
+  end
+
+  it "creates a lockrun directory in the configured source dir" do
+    chef_run.should create_directory("/usr/local/othersrc/lockrun").with(
+      :owner     => "root",
+      :group     => "root",
+      :mode      => 0755,
+      :recursive => true      
+    )
+  end
+  
+  it "copies the lockrun.c source file to the source dir" do
+    chef_run.should create_file("/usr/local/othersrc/lockrun/lockrun.c").with(
+      :owner  => "root",
+      :group  => "root", 
+      :mode    => 0644
+    )
+  end
+  
+  context "when lockrun is installed already" do
+    before(:each) do
+      stub_command("which lockrun").and_return(true)
+    end
+
+    it "does not compile" do
+      chef_run.should_not run_execute("gcc lockrun.c -o lockrun")
+    end
+  end
+  
+  context "when lockrun is not installed" do
+    before(:each) do
+      stub_command("which lockrun").and_return(false)
+    end
+    
+    it "compiles lockrun from source" do
+      chef_run.should run_execute("gcc lockrun.c -o lockrun").with(
+        :cwd  => "/usr/local/othersrc/lockrun",
+        :user => "root"        
+      )
+    end
+  end
+  
+  it "links the executable from /usr/local/bin" do
+    chef_run.should create_link("/usr/local/bin/lockrun").with(
+      :target_file => "/usr/local/othersrc/lockrun/lockrun"
+    )
+  end
+  
+end
