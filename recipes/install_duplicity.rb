3c814d3e15e304e4544bf90307e9dcf23f0661e8
Andrew Coulton <andrew@ingenerator.com>
2014-01-30 22:01:49 +0000

    Install duplicity from source
    Gives us the best control of the version - the official package
    archives are very out of date.

---
+++ recipes/install_duplicity.rb
@@ -0,0 +1,53 @@
+#
+# Installs duplicity from source
+#
+# Author::  Andrew Coulton (<andrew@ingenerator.com>)
+# Cookbook Name:: duplicity-backup
+# Recipe:: install_duplicity
+#
+# Copyright 2013-14, inGenerator Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install the dependencies for build and for all the various backends
+package "ncftp"
+package "python-paramiko"
+package "python-pycryptopp"
+package "lftp"
+package "python-boto"
+
+src_name = File.basename(node['duplicity']['src_url'])
+src_path = File.join(node['duplicity']['src_dir'], src_name)
+
+remote_file src_path do
+  action   :create_if_missing
+  source   node['duplicity']['src_url']
+  owner    "root"
+  group    "root"
+  mode     0644
+  backup   false
+  notifies :run, "execute[install-duplicity]", :immediately
+end
+
+# Always build if the executable isn't there - could be a previous failed provision
+default_action = Kernel.system('which duplicity') ? :nothing : :run
+unpack_dir_name = File.basename(src_name, '.tar.gz')
+
+execute "install-duplicity" do
+  action   default_action
+  command  "tar xf #{src_name} && cd #{unpack_dir_name} && python setup.py install"
+  user     "root"
+  group    "root"
+  cwd      node['duplicity']['src_dir']  
+end
---
+++ spec/install_duplicity_spec.rb
@@ -0,0 +1,107 @@
+require 'spec_helper'
+
+describe 'duplicity-backup::install_duplicity' do
+  let (:chef_run) do
+    ChefSpec::Runner.new do | node |
+      # Set non-standard attributes to check the recipe is using the attributes
+      node.set['duplicity']['src_url'] = 'http://code.launchpad.net/duplicity/0.6-series/0.6.22/+download/duplicity-0.6.22.tar.gz'
+      node.set['duplicity']['src_dir'] = '/usr/local/othersrc'
+    end.converge(described_recipe)
+  end    
+
+  it "installs ncftp" do
+    chef_run.should install_package "ncftp"
+  end
+
+  it "installs python-paramiko" do
+    chef_run.should install_package "python-paramiko"
+  end  
+  
+  it "installs python-pycryptopp" do
+    chef_run.should install_package "python-pycryptopp"
+  end
+
+  it "installs lftp" do
+    chef_run.should install_package "lftp"
+  end
+  
+  it "installs python-boto" do
+    chef_run.should install_package "python-boto"
+  end
+  
+  it "installs python-dev" do
+    chef_run.should install_package "python-boto"
+  end
+  
+  it "installs librsync-dev" do
+    chef_run.should install_package "python-boto"
+  end
+  
+  it "fetches the remote source if missing" do
+    chef_run.should create_remote_file_if_missing("/usr/local/othersrc/duplicity-0.6.22.tar.gz").with(
+      :source => "http://code.launchpad.net/duplicity/0.6-series/0.6.22/+download/duplicity-0.6.22.tar.gz"
+    )
+  end
+  
+  it "sets the source archive owned by root with 0644" do
+    chef_run.should create_remote_file_if_missing("/usr/local/othersrc/duplicity-0.6.22.tar.gz").with(
+      :owner => "root",
+      :group => "root",
+      :mode  => 0644
+    )
+  end
+  
+  it "does not take backups of the source archive" do
+    chef_run.should create_remote_file_if_missing("/usr/local/othersrc/duplicity-0.6.22.tar.gz").with(
+      :backup => false
+    )    
+  end
+  
+  context "when a new source file is downloaded" do
+    it "triggers an immediate unpack and build" do
+      resource = chef_run.remote_file("/usr/local/othersrc/duplicity-0.6.22.tar.gz")
+      resource.should notify('execute[install-duplicity]').to(:run).immediately
+    end
+  end
+  
+  context "when the source is unchanged and the executable is present" do
+    before(:each) do
+      Kernel.stub(:system).with('which duplicity').and_return(true)
+    end
+
+    it "does not attempt to unpack and build the source" do
+      chef_run.should_not run_execute('install-duplicity')
+    end
+  end
+  
+  context "when the duplicity executable is not present even if the source is unchanged" do
+    before(:each) do
+      Kernel.stub(:system).with('which duplicity').and_return(false)
+    end
+    
+    it "compiles and installs from source" do
+      chef_run.should run_execute('install-duplicity')
+    end
+    
+    it "uses the correct command to unpack and build" do
+      chef_run.should run_execute('install-duplicity').with(
+        :command => "tar xf duplicity-0.6.22.tar.gz && cd duplicity-0.6.22 && python setup.py install"
+      )
+    end
+    
+    it "builds as root" do
+      chef_run.should run_execute('install-duplicity').with(
+        :user  => "root",
+        :group => "root"
+      )
+    end
+    
+    it "executes the build command in the source dir" do
+      chef_run.should run_execute('install-duplicity').with(
+        :cwd  => "/usr/local/othersrc"
+      )
+    end    
+    
+  end  
+  
+end
