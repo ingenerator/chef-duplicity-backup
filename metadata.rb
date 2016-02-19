6cfa320b4fee609fdd1879b0f874e577cbba4a77
Andrew Coulton <andrew@ingenerator.com>
2016-02-19 11:56:12 +0000

    Update version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrew@ingenerator.com'
 license 'Apache 2.0'
 description 'Installs and configures duplicity for remote backup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '1.0.1'
 
 %w(ubuntu).each do |os|
   supports os
