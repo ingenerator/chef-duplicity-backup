6ee18f366797402e9c284249643548434211be93
Andrew Coulton <andrew@ingenerator.com>
2017-02-16 03:19:47 +0000

    Update build dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,9 @@ require 'support/matchers'
 RSpec.configure do |c|
   c.filter_run(focus: true)
   c.run_all_when_everything_filtered = true
+
+  # Default platform / version to mock Ohai data from
+  c.platform = 'ubuntu'
+  c.version = '14.04'
+
 end
