73b7f6d0a17b91c58b3b0c8f8c388f04a3e0fb41
acoulton <andrew@ingenerator.com>
2021-04-02 13:26:33 +0100

    Pin to a 2.7-compatible URL for get-pip.py
    Seems like pypa is now serving a version that needs python 3.6,
    pin to the older release.

--- recipes/install_duplicity.rb
+++ recipes/install_duplicity.rb
@@ -26,7 +26,7 @@ python_runtime '2.7' do
   # the internal check to see if the url needs to be switched to a python 2.6 compatible one. That in turn avoids
   # poise-python attempting to parse an operating system package version as a gem version string, which currently
   # throws an exception as the debian python version 2.7.15+ is not a valid gem version.
-  get_pip_url 'https://bootstrap.pypa.io/get-pip.py#skip-poise-python-2.6-check'
+  get_pip_url 'https://bootstrap.pypa.io/pip/2.7/get-pip.py#skip-poise-python-2.6-check'
 end
 python_package 'lockfile'
 python_package 'fasteners'
